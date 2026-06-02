0fa612506709d3563245e9201427cf0a3a922745
protocodeinnovations-boop <protocodeinnovations@gmail.com>
2026-06-02 22:49:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-print("Asthra Quiz Bot Started")
+import os
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="🎉 Asthra Quiz Bot Connected Successfully!"
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
