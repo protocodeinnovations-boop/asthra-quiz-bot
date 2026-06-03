fe72f7a50216368ee1a8d24165e0f77f42e2664f
protocodeinnovations-boop <protocodeinnovations@gmail.com>
2026-06-03 16:10:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ async def main():
 
     print("STATUS:", response.status_code)
     print("FINAL URL:", response.url)
-    print("TEXT:", response.text[:1000])
 
     response.raise_for_status()
 
@@ -42,13 +41,10 @@ async def main():
             q["d"]
         ],
         type="quiz",
-        correct_option_id=["A", "B", "C", "D"].index(q["correct"])
-    )
-
-    await bot.send_message(
-        chat_id=CHAT_ID,
-        text=f"📘 Explanation:\n{q['explanation']}"
+        correct_option_id=["A", "B", "C", "D"].index(q["correct"]),
+        explanation=q["explanation"]
     )
 
 if __name__ == "__main__":
+    asyncio.run(main())
     asyncio.run(main())
