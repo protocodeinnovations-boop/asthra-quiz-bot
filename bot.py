f98d90e06486324873179c9ce131c01a7eaf1b94
protocodeinnovations-boop <protocodeinnovations@gmail.com>
2026-06-03 13:50:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,37 @@
-import os
-import asyncio
-import requests
-from telegram import Bot
+if not CHAT_ID:
+    raise ValueError("CHAT_ID not found in Railway Variables")
 
-BOT_TOKEN = os.getenv("8539465341:AAF3GakskKFGxT5hWQV1pIQgnx5-XF0IqH0")
-CHAT_ID = os.getenv("1001234567890")
+bot = Bot(token=BOT_TOKEN)
 
-API_URL = "https://script.google.com/macros/s/AKfycbxBWEHGX_zzsg2oTmImd3z9K7aX9U9KvO72uiAEEgA9bvbk_RP6vdvu80JiREGNMIoD/exec"
+response = requests.get(API_URL, allow_redirects=True)
 
-async def main():
-    bot = Bot(token=8539465341:AAF3GakskKFGxT5hWQV1pIQgnx5-XF0IqH0)
+print("STATUS:", response.status_code)
+print("URL:", response.url)
+print("TEXT:", response.text[:1000])
 
-    response = requests.get(API_URL, allow_redirects=True)
+response.raise_for_status()
 
-    print("STATUS:", response.status_code)
-    print("URL:", response.url)
-    print("TEXT:", response.text[:1000])
+questions = response.json()
 
-    questions = response.json()
+if not questions:
+    raise ValueError("No questions returned from API")
 
-    q = questions[0]
+q = questions[0]
 
-    await bot.send_poll(
-        chat_id=CHAT_ID,
-        question=q["question"],
-        options=[
-            q["a"],
-            q["b"],
-            q["c"],
-            q["d"]
-        ],
-        type="quiz",
-        correct_option_id=["A", "B", "C", "D"].index(q["correct"])
-    )
+await bot.send_poll(
+    chat_id=CHAT_ID,
+    question=q["question"],
+    options=[
+        q["a"],
+        q["b"],
+        q["c"],
+        q["d"]
+    ],
+    type="quiz",
+    correct_option_id=["A", "B", "C", "D"].index(q["correct"])
+)
 
-    await bot.send_message(
-        chat_id=,1001234567890
-        text=f"📘 Explanation:\n{q['explanation']}"
-    )
-
-if __name__ == "__main__":
-    asyncio.run(main())
+await bot.send_message(
+    chat_id=CHAT_ID,
+    text=f"📘 Explanation:\n{q['explanation']}"
+)
